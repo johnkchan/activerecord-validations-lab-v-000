b3b1d8d9497f970ef4ee6de786254970e0899271
Jonathan Chan <johnkchan252@gmail.com>
2019-02-02 19:49:22 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
   validates :phone_number, length: { is: 10 }
 end
